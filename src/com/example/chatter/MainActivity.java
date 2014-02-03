998e9f28e89899448c84c6ed9618863b1822ce84
Niger Little-Poole <nl2418@columbia.edu>
2014-02-03 13:04:25 -0500

    Added initial Bluetooth Support. App checks if bluetooth is on and asks the user to turn it on if it is off

--- src/com/example/chatter/MainActivity.java
+++ src/com/example/chatter/MainActivity.java
@@ -2,16 +2,50 @@ package com.example.chatter;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    @Override
+    private static final int REQUEST_ENABLE_BT = 1;
+
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BluetoothAdapter mBluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
+        if(mBluetoothAdapter ==null){
+        	Context context = getApplicationContext();
+        	CharSequence text = "No bluetooth";
+        	int duration = Toast.LENGTH_SHORT;
+        	Toast toast = Toast.makeText(this, text, duration);
+        	toast.show();
+        }
+        else if (!mBluetoothAdapter.isEnabled()){
+        	Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
     }
-
+	// Create a BroadcastReceiver for ACTION_FOUND
+	private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+	    public void onReceive(Context context, Intent intent) {
+	        String action = intent.getAction();
+	        // When discovery finds a device
+	        if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+	            // Get the BluetoothDevice object from the Intent
+	            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+	            // Add the name and address to an array adapter to show in a ListView
+	            //mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
+	        }
+	    }
+	};
+	// 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
