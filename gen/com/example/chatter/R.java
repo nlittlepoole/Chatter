50f9dbf219253768e67ed015add2b910c2c0d2c0
Niger Little-Poole <nl2418@columbia.edu>
2014-02-09 15:17:13 -0500

    Changed Name/Package , Added Splash, Changed Bluetooth Enable Flow

--- gen/com/example/chatter/R.java
+++ gen/com/example/chatter/R.java
@@ -41,14 +41,16 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_notify=0x7f030001;
-        public static final int chat=0x7f030002;
-        public static final int login=0x7f030003;
-        public static final int main=0x7f030004;
-        public static final int splash=0x7f030005;
+        public static final int activity_splash=0x7f030002;
+        public static final int chat=0x7f030003;
+        public static final int login=0x7f030004;
+        public static final int main=0x7f030005;
+        public static final int splash=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f070000;
         public static final int notify=0x7f070001;
+        public static final int splash=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -56,6 +58,7 @@ public final class R {
         public static final int hello_world=0x7f050002;
         public static final int title_activity_main=0x7f050003;
         public static final int title_activity_notify=0x7f050004;
+        public static final int title_activity_splash=0x7f050005;
     }
     public static final class style {
         /** 
